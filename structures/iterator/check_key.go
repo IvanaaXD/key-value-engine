81e76b330190f91bf21af1bab8b61d40a5a50525
Ivana_R <ivanar1357@gmail.com>
2024-02-07 17:04:53 +0100

    my structs

---
+++ structures/iterator/check_key.go
@@ -0,0 +1,43 @@
+package iterator
+
+import (
+	"bytes"
+	"github.com/IvanaaXD/NASP---Projekat/config"
+)
+
+func IsInvalidKey(i Iterator) bool {
+
+	if i != nil && i.Value() != nil && !IsSpecialKey([]byte(i.Value().Key)) {
+		return false
+
+	}
+	return true
+}
+
+func IsSpecialKey(key []byte) bool {
+
+	specialPrefixes := [][]byte{
+		[]byte(config.BF_PREFIX),
+		[]byte(config.CMS_PREFIX),
+		[]byte(config.HLL_PREFIX),
+		[]byte(config.SH_PREFIX),
+	}
+
+	for _, spec := range specialPrefixes {
+		if bytes.HasPrefix(key, spec) {
+			return true
+		}
+	}
+
+	specialKeys := [][]byte{
+		[]byte(config.COMPRESSION_DICT),
+		[]byte(config.RATE_LIMIT),
+	}
+
+	for _, spec := range specialKeys {
+		if bytes.Equal(key, spec) {
+			return true
+		}
+	}
+	return false
+}
---
+++ structures/iterator/iter.go
@@ -0,0 +1,12 @@
+package iterator
+
+import "github.com/IvanaaXD/NASP---Projekat/record"
+
+// b-tree, hash-map or skip-list
+
+type Iterator interface {
+	SetIndex()
+	Next() bool
+	Value() *record.Record
+	Stop() bool
+}
